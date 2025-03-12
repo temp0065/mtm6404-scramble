f08e322403e4f5319e6afe77a56d24b05811af02
Nathan Templeton <temp0065@algonquinlive.com>
2025-03-12 16:04:14 -0400

    Finishes most of code

--- app.js
+++ app.js
@@ -47,16 +47,46 @@ const initialWords = [
   "THEOCRACY"
 ];
 
-const PassBtn = ({ passes }) => {
+const ProgressBar = ({ progress }) => {
+  console.log(progress)
   return (
-    <button type="button" className="button is-warning" disabled={!passes}>
+    <progress className="progress is-info" value={!progress ? 100 : Math.abs(((progress - 10) * 10))} max="100"></progress>
+  )
+}
+
+const GuessAlert = ({ children, correct }) => {
+
+  if (correct !== null) return (
+    <div className={correct ? "message is-success" : "message is-danger"}>
+      <div className="message-header" style={{borderRadius: "0.375rem"}}>
+        {children}
+      </div>
+    </div>
+  )
+
+  /*
+  // Ensure the value is not null
+
+  if (correct !== null) return (
+    <div className={correct ? "message is-success" : "message is-danger"}>
+      <div className="message-header">
+        <p>You guessed {correct ? "Correct :)" : "Wrong ):"}</p>
+      </div>
+    </div>
+  )
+  */
+}
+
+const PassBtn = ({ passes, onPass, gameOver }) => {
+  return (
+    <button type="button" className="button is-warning is-outlined is-align-self-center" disabled={!passes || gameOver} onClick={onPass}>
       {passes} Passes Remaining
     </button>
   )
 };
 
 
-const ScrabbleForm = ({ onWordGuess, gameOver }) => {
+const ScrambleForm = ({ onWordGuess, gameOver }) => {
   const [guess, setGuess] = React.useState("");
 
   const handleFormChange = (e) => {
@@ -74,7 +104,8 @@ const ScrabbleForm = ({ onWordGuess, gameOver }) => {
   return (
     <form onSubmit={handleFormSubmit} className="field is-grouped">
       <div className="control is-expanded">
-        <input type="text" className="input" value={guess} onChange={handleFormChange} disabled={gameOver} />
+        <label htmlFor="guess" hidden>Guess The Word</label>
+        <input type="text" className="input" value={guess} onChange={handleFormChange} disabled={gameOver} name="guess" id="guess" />
       </div>
       <div>
         <button type="submit" className="button is-primary" disabled={gameOver}>Submit Guess</button>
@@ -83,47 +114,56 @@ const ScrabbleForm = ({ onWordGuess, gameOver }) => {
   )
 };
 
-const ScrabbleWord = ({ word }) => {
+const ScrambleWord = ({ word }) => {
   return <h2 className="title is-2 has-text-centered">{word}</h2>
 };
 
-const ScrabbleScore = ({ type, typeAccumulated }) => {
+const ScrambleScore = ({ type, typeAccumulated }) => {
   return (
-    <div>
-      <h3>{typeAccumulated}</h3>
-      <p>{type}</p>
+    <div className="level-item has-text-centered">
+      <div>
+        <h3 className="heading">{typeAccumulated}</h3>
+        <p className="title">{type}</p>
+      </div>
     </div>
   )
 };
 
-const ScrabbleScoreboard = ({ points, strikes }) => {
+const ScrambleScoreboard = ({ points, strikes }) => {
 
   return (
-    <div className="is-flex is-justify-content-space-between">
-      <ScrabbleScore type="Points" typeAccumulated={points} />
-      <ScrabbleScore type="Strikes" typeAccumulated={strikes} />
+    <div className="level">
+      <ScrambleScore type="Points" typeAccumulated={points} />
+      <ScrambleScore type="Strikes" typeAccumulated={strikes} />
     </div>
   )
 };
 
 const App = () => {
   const [playerData, setPlayerData] = React.useState(() => {
+    // Get saved values
     const savedValues = JSON.parse(localStorage.getItem("playerData"));
+    // If there are saved values on ls make them the states
     if (savedValues) {
       return savedValues;
     }
+    // If not create the states
     else {
+      const newWordList = shuffle(initialWords);
+
       return {
-        words: shuffle(initialWords),
+        words: newWordList,
+        word: shuffle(newWordList[0]),
         numPasses: 3,
         strikes: 0,
         points: 0,
         gameOver: false,
-        guessType: null
+        guessCorrect: null
       };
     }
   })
 
+  console.log(playerData.words)
   const maxStrikes = 3;
 
   // Updates local storage when playerData is modified
@@ -133,21 +173,29 @@ const App = () => {
 
   const handleWordGuess = (guess) => {
 
+    // If the game is over than do not allow it to progress
     if (playerData.gameOver) return;
 
     if (guess.toLowerCase() === playerData.words[0].toLowerCase()) {
-      setPlayerData(prevData => ({
-        ...prevData,
-        words: prevData.words.slice(1),
-        points: prevData.points + 1,
-        guessType: 0
-      }));
+
+      setPlayerData(prevData => {
+        const newWordList = prevData.words.slice(1);
+        const newWordListLength = newWordList.length;
+        return {
+          ...prevData,
+          words: newWordList,
+          word: newWordListLength ? shuffle(newWordList[0]) : "",
+          points: prevData.points + 1,
+          guessCorrect: 1,
+          gameOver: newWordListLength ? false : true
+        }
+      });
     }
     else {
       setPlayerData(prevData => ({
         ...prevData,
         strikes: prevData.strikes + 1,
-        guessType: -1
+        guessCorrect: 0
       }));
     }
 
@@ -160,25 +208,50 @@ const App = () => {
   }
 
   const handlePlayAgain = () => {
+    const newWordList = shuffle(initialWords);
+
     setPlayerData(() => ({
-      words: shuffle(initialWords),
+      words: newWordList,
+      word: shuffle(newWordList[0]),
       numPasses: 3,
       strikes: 0,
       points: 0,
-      guessType: null,
-      gameOver: false
+      gameOver: false,
+      guessCorrect: null
     }));
   }
 
+  const handlePass = () => {
+    // Do not allow pass if no more passes
+    if (!playerData.numPasses) return;
+
+    // Lower the number of passes by 1 and shuffle the word again
+    setPlayerData(prevData => {
+
+      const newWordList = shuffle(prevData.words);
+
+      return {
+        ...prevData,
+        words: newWordList,
+        word: newWordList[0],
+        numPasses: prevData.numPasses - 1,
+        guessCorrect: null
+      }
+    });
+  }
+
   return (
-    <main className="container is-max-desktop">
-      <h1 className="title is-1 has-text-white has-text-centered">Welcome To Scrabble</h1>
-      <ScrabbleScoreboard points={playerData.points} strikes={playerData.strikes} />
-      <ScrabbleWord word={shuffle(playerData.words[0])} />
-      <ScrabbleForm onWordGuess={handleWordGuess} gameOver={playerData.gameOver} />
+    <main className="container is-max-desktop is-flex is-flex-direction-column is-justify-content-center p-5" style={{ height: "100vh" }}>
+      <h1 className="title is-1 has-text-white has-text-centered has-text-primary">Welcome To Scramble</h1>
+      <p class="subtitle is-4 has-text-centered">Guess All The Scrambled Words To Win</p>
+      <ProgressBar progress={playerData.words.length} />
+      <ScrambleScoreboard points={playerData.points} strikes={playerData.strikes} />
+      {playerData.guessCorrect !== null && <GuessAlert correct={playerData.guessCorrect}>{playerData.guessCorrect ? "Correct" : "Wrong"}</GuessAlert>}
+      <ScrambleWord word={playerData.word} />
+      <ScrambleForm onWordGuess={handleWordGuess} gameOver={playerData.gameOver} />
       <div className="is-flex is-flex-direction-column is-justify-content-center">
-        <PassBtn passes={playerData.numPasses} />
-        {playerData.gameOver && <button className="button is-info" onClick={handlePlayAgain}>Play Again</button>}
+        <PassBtn passes={playerData.numPasses} onPass={handlePass} gameOver={playerData.gameOver} />
+        {playerData.gameOver && <button className="button is-info is-align-self-center mt-3" onClick={handlePlayAgain}>Play Again</button>}
       </div>
     </main>
   );
